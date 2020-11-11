c0901f632718172bf30ca51887607a95e2fa9338
pvh85m <pvh855@gmail.com>
2020-11-12 00:38:14 +0300

    end of part_1

--- part_1/index.js
+++ part_1/index.js
@@ -1,17 +1,65 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
-  res.write('<h1>He</h1>')
-  res.write('<h2>He</h2>')
+    if (req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html;charset=utf-8'
+        })
 
-  //конец респонса, обязателен. может быть пустым
-  res.end('<div>finish</div>')
+        if (req.url === '/') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'index.html'),
+                'utf-8',
+                (err, data) => {
+                    if (err) throw err
+                    res.end(data)
+                }
+            )
+        } else if (req.url === '/another') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'another_page.html'),
+                'utf-8',
+                (err, data) => {
+                    if (err) throw err
+                    res.end(data)
+                }
+            )
+        } else if (req.url === '/api/users'){
+            res.writeHead(200,{
+                'Content-Type':'text/json'
+            })
+
+            const users = [
+                {user:"vasya"},
+                {user:"gena"}
+            ]
+            res.end(JSON.stringify(users))
+        }
+    } else if (req.method === 'POST') {
+        const body = []
+
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        })
+
+        req.on('end', () => {
+
+            res.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8'
+            })
+            const msg = body.toString()
+            res.end(`
+                <h1>text: ${msg}</h1>
+            `)
+        })
+
+    }
 })
 
 server.listen(8888, () => {
     console.log('server started')
-}
-)
+})
